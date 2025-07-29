dd63ec0b2be4c4ff596f6c8bfea600c3e0d9a028
Oliver Levine <oliversfphillips@icloud.com>
2025-07-29 09:36:14 -0700

    Formatted

--- controllers/users.js
+++ controllers/users.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const User = require('../models/users.js');
 
 router.get('/', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.render('users/index', { users });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const users = await User.find({});
+        res.render('users/index', { users });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.get('/:userId', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    res.render('users/show', { user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const user = await User.findById(req.params.userId);
+        res.render('users/show', { user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = router;
