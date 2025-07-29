45f5e4a684dfb3dffcea3a69dfc751c902adb1e9
Oliver Levine <oliversfphillips@icloud.com>
2025-07-29 09:35:07 -0700

    Formatted

--- controllers/vinyls.js
+++ controllers/vinyls.js
@@ -3,63 +3,63 @@ const router = express.Router();
 const VinylVault = require('../models/vinyls.js');
 
 router.get('/', async (req, res) => {
-  try {
-    const vinyls = await VinylVault.find({ owner: req.session.user._id });
-    res.render('vinyls/index', { vinyls });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-    res.redirect('/vinyls');
-  }
+    try {
+        const vinyls = await VinylVault.find({ owner: req.session.user._id });
+        res.render('vinyls/index', { vinyls });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+        res.redirect('/vinyls');
+    }
 });
 
 router.get('/new', (req, res) => {
-  res.render('vinyls/new');
+    res.render('vinyls/new');
 });
 
 router.post('/', async (req, res) => {
-  try {
-    const newVinyl = new VinylVault({ ...req.body, owner: req.session.user._id });
-    await newVinyl.save();
-    res.redirect('/vinyls');
-  } catch (error) {
-    console.error(error);
-    res.status(400).send('Bad Request');
-    res.redirect('/vinyls');
-  }
+    try {
+        const newVinyl = new VinylVault({ ...req.body, owner: req.session.user._id });
+        await newVinyl.save();
+        res.redirect('/vinyls');
+    } catch (error) {
+        console.error(error);
+        res.status(400).send('Bad Request');
+        res.redirect('/vinyls');
+    }
 });
 
 router.get('/:vinylId/edit', async (req, res) => {
-  try {
-    const vinyl = await VinylVault.findById(req.params.vinylId, { owner: req.session.user._id });
-    res.render('vinyls/edit', { vinyl });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-    res.redirect('/vinyls');
-  }
+    try {
+        const vinyl = await VinylVault.findById(req.params.vinylId, { owner: req.session.user._id });
+        res.render('vinyls/edit', { vinyl });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+        res.redirect('/vinyls');
+    }
 });
 
 router.put('/:vinylId', async (req, res) => {
-  try {
-    await VinylVault.findByIdAndUpdate(req.params.vinylId, { owner: req.session.user._id, ...req.body });
-    res.redirect('/vinyls');
-  } catch (error) {
-    console.error(error);
-    res.status(400).send('Bad Request');
-    res.redirect('/vinyls');
-  }
+    try {
+        await VinylVault.findByIdAndUpdate(req.params.vinylId, { owner: req.session.user._id, ...req.body });
+        res.redirect('/vinyls');
+    } catch (error) {
+        console.error(error);
+        res.status(400).send('Bad Request');
+        res.redirect('/vinyls');
+    }
 });
 
 router.delete('/:vinylId', async (req, res) => {
-  try {
-    await VinylVault.findByIdAndDelete(req.params.vinylId, { owner: req.session.user._id });
-    res.redirect('/vinyls');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-    res.redirect('/vinyls');
-  }
+    try {
+        await VinylVault.findByIdAndDelete(req.params.vinylId, { owner: req.session.user._id });
+        res.redirect('/vinyls');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+        res.redirect('/vinyls');
+    }
 });
 
 module.exports = router;
