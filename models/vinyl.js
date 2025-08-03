c9675894c3752fed66c7d400f95c1b769689ce4b
Oliver Levine <oliversfphillips@icloud.com>
2025-08-02 18:35:58 -0700

    Looked into Discogs API integration and added environment variables for API keys and base URL. Found it very complicated to set up, so I will need to revisit this later. But I will probably use Spotify API instead for music data.

--- models/vinyl.js
+++ models/vinyl.js
@@ -10,19 +10,6 @@ const VinylVaultSchema = new mongoose.Schema({
     wishlist: { type: Boolean, default: false },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     
-    // Enhanced fields for Discogs integration
-    discogsId: { 
-        type: String,
-        index: true // For faster lookups when searching by Discogs ID
-    },
-    discogsData: {
-        masterId: { type: String },
-        catalogNumber: { type: String },
-        label: { type: String },
-        country: { type: String },
-        released: { type: String },
-        },
-    }
-,);
+});
 
 module.exports = mongoose.model("VinylVault", VinylVaultSchema);
